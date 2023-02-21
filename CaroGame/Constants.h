1d8d724e6aaf7f53b3a280c36f25437bcbdb70e4
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2023-02-21 20:50:35 +0700

    Update Constants.h

--- CaroGame/Constants.h
+++ CaroGame/Constants.h
@@ -4,10 +4,4 @@ namespace Constants {
 	const int PLAYER_AI = -1, PLAYER_HUMAN = 1;
 	const int WIN_VALUE_COUNT = 3;
 	const int INF = 1000, NULL_VALUE = -5;
-
-	const int BOARD_SIZE = 3;
-
-	const int BOARD_ROW_NUM= BOARD_SIZE + 1, BOARD_COLUMN_NUM = BOARD_ROW_NUM;
-	const int CELL_WIDTH = 6, CELL_HEIGHT = CELL_WIDTH - 2;
-	const int X_PIVOT = 10, Y_PIVOT = 0;
 }
