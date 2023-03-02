2d5174030d9780010d960cadee10ea9a0f7b81ec
Nhật Phước <vonhatphuoc32@gmail.com>
2023-03-02 21:51:48 +0700

    Audio changes + AmongUs changes

--- CaroGame/MainMenu.h
+++ CaroGame/MainMenu.h
@@ -6,7 +6,7 @@
 #include "Language.h"
 using namespace std;
 namespace MainMenu {
-void mauve(int x, int y, View::Color mat, View::Color mau){};       // Pain Avatar
+void mauve(int x, int y, View::Color mat, View::Color mau){};       // Paint Among us
 	void Logo(int x, int y);                                        // Paint Logo
 	void ScreenMainMenu(NavigationHost& NavHost);                   // Paint Main Menu
 }
